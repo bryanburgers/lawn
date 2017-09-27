826de323d09a21fd54ec735b19048b414eb7d14f
Bryan Burgers <bryan@burgers.io>
2017-09-27 07:09:17 -0500

    refactor(lawn): Add helpers: props, validation, transformation
    Add three helpers called 'props', 'validation', and 'transformation' as the basic building blocks,
    and build the other configurations off of that. This replaces the `createSymbol` that was pretty
    messy.

--- index.js
+++ index.js
@@ -1,63 +1,76 @@
 'use strict'
 
 const internalsSymbol = Symbol('Lawn config')
-const validatorSymbol = Symbol('Lawn validators')
-const convertersSymbol = Symbol('Lawn converters')
-const createSymbol = Symbol('Lawn create')
+const validatorsSymbol = Symbol('Lawn validators')
+const transformationsSymbol = Symbol('Lawn transformations')
+const cloneSymbol = Symbol('Lawn create')
 
 const Lawn = {
   [internalsSymbol]: {},
-  [validatorSymbol]: [],
-  [convertersSymbol]: [],
+  [validatorsSymbol]: [],
+  [transformationsSymbol]: [],
 
-  [createSymbol] (changes, validator, converter) {
+  [cloneSymbol] (changes, validator, converter) {
     const obj = Object.create(Lawn)
-    obj[internalsSymbol] = Object.assign({}, this[internalsSymbol], changes)
-    if (validator) {
-      obj[validatorSymbol] = [...this[validatorSymbol], validator]
-    } else {
-      obj[validatorSymbol] = this[validatorSymbol]
-    }
-    if (converter) {
-      obj[convertersSymbol] = [...this[convertersSymbol], converter]
-    } else {
-      obj[convertersSymbol] = this[convertersSymbol]
-    }
+    obj[internalsSymbol] = this[internalsSymbol]
+    obj[validatorsSymbol] = this[validatorsSymbol]
+    obj[transformationsSymbol] = this[transformationsSymbol]
+    return obj
+  },
+
+  props (props) {
+    const obj = this[cloneSymbol]()
+    obj[internalsSymbol] = Object.assign({}, this[internalsSymbol], props)
+    return obj
+  },
+
+  validation (validation) {
+    const obj = this[cloneSymbol]()
+    obj[validatorsSymbol] = [...this[validatorsSymbol], validation]
+    return obj
+  },
+
+  transformation (transformation) {
+    const obj = this[cloneSymbol]()
+    obj[transformationsSymbol] = [...this[transformationsSymbol], transformation]
     return obj
   },
 
   get string () {
-    return this[createSymbol]({ type: 'string' })
+    return this.props({ type: 'string' })
   },
 
   get number () {
-    return this[createSymbol]({ type: 'number' }, { message: 'is not a number', fn: v => !Number.isNaN(v) }, v => parseInt(v, 10))
+    return this.props({ type: 'number' })
+      .transformation(v => parseInt(v, 10))
+      .validation({ message: 'is not a number', fn: v => !Number.isNaN(v) })
   },
 
   get bool () {
-    return this[createSymbol]({ type: 'bool' }, null, v => {
-      switch (v.toLowerCase()) {
-        case 'true':
-        case 'yes':
-        case 'y':
-        case '1':
-          return true
-        default:
-          return false
-      }
-    })
+    return this.props({ type: 'bool' })
+      .transformation(v => {
+        switch (v.toLowerCase()) {
+          case 'true':
+          case 'yes':
+          case 'y':
+          case '1':
+            return true
+          default:
+            return false
+        }
+      })
   },
 
   desc (description) {
-    return this[createSymbol]({ description })
+    return this.props({ description })
   },
 
   example (example) {
-    return this[createSymbol]({ example })
+    return this.props({ example })
   },
 
   default (def) {
-    return this[createSymbol]({ default: def })
+    return this.props({ default: def })
   },
 
   validate (config, properties) {
@@ -69,8 +82,8 @@ const Lawn = {
 
     for (const key of Object.keys(config)) {
       const cfg = config[key][internalsSymbol]
-      const converters = config[key][convertersSymbol]
-      const validators = config[key][validatorSymbol]
+      const transformations = config[key][transformationsSymbol]
+      const validators = config[key][validatorsSymbol]
       const originalVal = properties[key]
 
       let val
@@ -80,7 +93,7 @@ const Lawn = {
         }
         val = cfg.default
       } else {
-        val = converters.reduce((val, converter) => converter(val), originalVal)
+        val = transformations.reduce((val, transformation) => transformation(val), originalVal)
       }
 
       for (const validator of validators) {
